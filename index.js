354b355ff32f690878e8a0bcb3a22ce0427a863d
Cynthia <108278881+Ketienyei@users.noreply.github.com>
2022-10-19 04:46:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config()
 
 const express = require('express')
 const app = express()
@@ -21,7 +21,7 @@ const posts= [
 
 //route middleware
 app.get('/posts', (req,res) =>{
-    res.json(posts)
+    res.json(posts.filter(posts.username === req.user.name))
 })
 
 app.post('/login',(req, res) =>{
@@ -31,7 +31,20 @@ app.post('/login',(req, res) =>{
     const user ={name :username}
 
     const accessToken = jwt.sign(user,process.env.ACCESS_SECRET_SECRET)
-    res.json({accessToken:accessToken})
+    res.json({accessToken: accessToken})
 })
+ 
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers ['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return red.sendStatus(401)
+     
+    jwt.verify(token, process.env.ACCESS_SECRET_SECRET, (err, user) =>{
+        if(err) return res.sendStatus(403)
+        req.user =user
+        next()
+    })
+}
 
 app.listen(3000, () => console.log(`Server up and running`))
